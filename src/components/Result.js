2aab6623e635d86283dff21f188ad2bc9f185c18
Gaurang Ingale <gaurang.ingale@outlook.com>
2021-02-13 19:34:25 +0100

    Setup the layout for Result

--- src/components/Result.js
+++ src/components/Result.js
@@ -4,13 +4,21 @@ import "../styles/result.css";
 
 function Result(props) {
   return (
-    <div className="result">
+    <div className="outer">
       <h2>
         <strong>Title:</strong> Looper
       </h2>
-      <ResultDetail name="Year" value="2012" />
-      <ResultDetail name="Rated" value="R" />
-      <ResultDetail name="Released" value="28 Sep 2012" />
+      <div className="box">
+        <div className="result">
+          <ResultDetail name="Year" value="2012" />
+          <ResultDetail name="Rated" value="R" />
+          <ResultDetail name="Released" value="28 Sep 2012" />
+        </div>
+        <img
+          src="https://m.media-amazon.com/images/M/MV5BMTg5NTA3NTg4NF5BMl5BanBnXkFtZTcwNTA0NDYzOA@@._V1_SX300.jpg"
+          alt="Poster"
+        />
+      </div>
     </div>
   );
 }
